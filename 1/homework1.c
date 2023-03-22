1470e2c3378be305cc305db1214a98703cb35bba
Лев Черняховский <levcernahovskij4419@gmail.com>
2023-03-22 15:59:16 +0300

    Update homework1.c

--- 1/homework1.c
+++ 1/homework1.c
@@ -3,9 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-int print_bytes = 0;
-int print_lines = 0;
-int print_words = 0;
+short param_c_l_w = 0;
+short param_cnt = 0;
 
 void print_help(const char* program_name) {
     printf("Usage: %s [-C] [-L] [-W] [file1 file2 ...]\n", program_name);
@@ -17,18 +16,17 @@ void print_help(const char* program_name) {
 }
 void print_out(num_bytes, num_lines, num_words, name) {
     printf("%s:\t", name);
-    if (print_bytes) {
+    if (param_c_l_w&1) {
         printf("\tbyte count-%d\t", num_bytes);
     }
-    if (print_lines) {
+    if (param_c_l_w & (1 << 1)) {
         printf("\tline count-%d\t", num_lines);
     }
-    if (print_words) {
+    if (param_c_l_w & (1 << 2)) {
         printf("\tword count-%d\t", num_words);
     }
     printf("\n");
 }
-
 int main(int argc, char* argv[]) {
     int num_files = 0;
     FILE* file;
@@ -36,20 +34,18 @@ int main(int argc, char* argv[]) {
     int sum_bytes = 0;
     int sum_lines = 0;
     int sum_words = 0;
-
-    if (argc == 1) {
-        print_help(argv[0]);
-        return 0;
-    }
     for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "-C") == 0) {
-            print_bytes = 1;
+            param_c_l_w |= 1;
+            param_cnt++;
         }
         else if (strcmp(argv[i], "-L") == 0) {
-            print_lines = 1;
+            param_c_l_w |= 1 << 1;
+            param_cnt++;
         }
         else if (strcmp(argv[i], "-W") == 0) {
-            print_words = 1;
+            param_c_l_w |= 1 << 2;
+            param_cnt++;
         }
         else if (strcmp(argv[i], "-?") == 0) {
             print_help(argv[0]);
@@ -59,11 +55,16 @@ int main(int argc, char* argv[]) {
             num_files++;
         }
     }
-    for (int j = print_lines + print_words + print_bytes+1; j < argc; j++) {
+    if (argc == 1 || param_c_l_w==0) {
+        print_help(argv[0]);
+        return 0;
+    }
+    for (int j=param_cnt +1; j <= argc; j++) {
         int num_bytes = 0;
         int num_lines = 0;
         int num_words = 0;
-        if (strcmp(argv[1], "-") == 0 || num_files == 0) {
+        if (num_files != 0 && j == argc) break;
+        if (num_files == 0 || strcmp(argv[j], "-") == 0) {
             file = stdin;
         }
         else {
@@ -72,6 +73,7 @@ int main(int argc, char* argv[]) {
         while (fgets(line, sizeof(line), file)) {
             num_bytes += strlen(line);
             num_lines++;
+            if (strlen(line) == 1) break;
             for (int i = 0; line[i] != '\0'; i++) {
                 if (line[i] == ' ' || line[i] == '\t' || line[i] == '\n') {
                     num_words++;
@@ -81,9 +83,13 @@ int main(int argc, char* argv[]) {
         sum_bytes += num_bytes;
         sum_lines += num_lines;
         sum_words += num_words;
-        print_out(num_bytes, num_lines, num_words, argv[j]);
+        if (file == stdin) {
+            print_out(num_bytes, num_lines, num_words, "stdin");
+        }
+        else {
+            print_out(num_bytes, num_lines, num_words, argv[j]);
+        }
     }
-    print_out(sum_bytes, sum_lines, sum_words, "Total count");
-
+    if (num_files > 1) print_out(sum_bytes, sum_lines, sum_words, "Total count");
     return 0;
 }
